e4a6c27ff15efcafea32afcfd8b5377063cd8da4
Minku Singh <mailminkusingh@gmail.com>
2021-08-03 23:49:36 +0530

    cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Cart.css';
+
+class Cart extends Cart.component {
+    render() {
+        return(
+            <div className = "container">
+                <div className = "header"></div>
+
+                <div className = "buy"></div>
+
+                <div className = "product"></div>
+
+                <div className = "footer"></div>
+            </div>
+        );
+    }
+}
+
+export default Cart;
