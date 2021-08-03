b07d15690facfe28fce195f90bea353e91e82fd5
Minku Singh <mailminkusingh@gmail.com>
2021-08-04 00:32:52 +0530

    app js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Login from './../Login/Login'; 
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Cart from '../Cart/Cart';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return(
-    <Header />
     /* <Login /> */
+      <Cart />
     )
   }
 }
